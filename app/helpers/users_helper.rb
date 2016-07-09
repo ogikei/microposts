c0c8d22d90ca0450d5ffe2f3d0fedb7b32aa78c1
KeisukeOgiwara <keisuke.ogiwara.dev@gmail.com>
2016-07-10 00:50:29 +0900

    add sign sign up and user detail
    add sign sign up and user detail.

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,8 @@
+module UsersHelper
+  def gravatar_for(user, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravator_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravator_url, alt: user.name, class: "gravator")
+  end
+end
