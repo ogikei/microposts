4252242df4d45bfd19b39535b80207f18bd93b14
KeisukeOgiwara <keisuke.ogiwara.dev@gmail.com>
2016-07-14 22:45:56 +0900

    modified
    modified

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation, :region, :profile)
   end
 
   def set_message
